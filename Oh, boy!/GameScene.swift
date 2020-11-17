1d745ff2fabcedfafa10fb8b30827959f1a63e73
Tuiaara <tuyara_ok@mail.ru>
2020-11-17 18:04:07 +0300

    Рефакторинг. Добавила coin, bigCoin.

--- Oh, boy!/GameScene.swift
+++ Oh, boy!/GameScene.swift
@@ -11,13 +11,20 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    // Variables
+    var sound = true
+    
     // Texture
     var bgSkyTexture: SKTexture!
     var bgMountainTexture: SKTexture!
     var bgGroundTexture: SKTexture!
-    
     var flyHeroTex: SKTexture!
     var runHeroTex: SKTexture!
+    var coinTexture: SKTexture!
+    var bigCoinTexture: SKTexture!
+    var coinAnimateTexture: SKTexture!
+    var bigCoinAnimateTexture: SKTexture!
+    
     
     // Sprite Nodes
     var bgSky = SKSpriteNode()
@@ -26,6 +33,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var ground = SKSpriteNode()
     var sky = SKSpriteNode()
     var hero = SKSpriteNode()
+    var coin = SKSpriteNode()
+    var bigCoin = SKSpriteNode()
     
     // Sprites Objects
     var skyBgObject = SKNode()
@@ -34,14 +43,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var groundObject = SKNode()
     var skyObject = SKNode()
     var heroObject = SKNode()
+    var coinObject = SKNode()
+    var bigCoinObject = SKNode()
     
     // Bit masks
     var heroGroup: UInt32 = 0x1 << 1
     var groundGroup: UInt32 = 0x1 << 2
+    var coinGroup: UInt32 = 0x1 << 3
+    var bigCoinGroup: UInt32 = 0x1 << 4
     
     // Textures array for animateWithTexture
     var heroflyTexturesArray = [SKTexture]()
     var heroRunTexturesArray = [SKTexture]()
+    var coinTexturesArray = [SKTexture]()
+    
+    //Timers
+    var timerAddCoin = Timer()
+    var timerAddBigCoin = Timer()
+    
+    // Sounds
+    var pickCoinPreload = SKAction()
     
     override func didMove(to view: SKView) {
         // Background texture
@@ -53,9 +74,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         flyHeroTex = SKTexture(imageNamed: "jump_fall.png")
         runHeroTex = SKTexture(imageNamed: "run_1.png")
         
+        // Coin texture
+        coinTexture = SKTexture(imageNamed: "Coin0.png")
+        bigCoinTexture = SKTexture(imageNamed: "Coin0.png")
+        coinAnimateTexture = SKTexture(imageNamed: "Coin0.png")
+        bigCoinAnimateTexture = SKTexture(imageNamed: "Coin0.png")
+        
+        coinTexturesArray = [SKTexture(imageNamed: "Coin0.png"),
+                            SKTexture(imageNamed: "Coin1.png"),
+                                    SKTexture(imageNamed: "Coin2.png"),
+                                    SKTexture(imageNamed: "Coin3.png")]
+        
         self.physicsWorld.contactDelegate = self
         
         createObjects()
+        
+        pickCoinPreload = SKAction.playSoundFileNamed("pickCoin.mp3", waitForCompletion: false)
+        
         createGame()
     }
     
@@ -68,13 +103,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(skyObject)
         self.addChild(groundObject)
         self.addChild(heroObject)
+        self.addChild(coinObject)
+        self.addChild(bigCoinObject)
     }
     
     func createGame() {
         createBg()
         createGround()
         createSky()
-        createHero()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            self.createHero()
+            self.addTimer()
+        }
     }
     
     func createBg() {
@@ -156,7 +197,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         hero.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: hero.size.width - 40, height: hero.size.height - 30))
         
         hero.physicsBody?.categoryBitMask = heroGroup
-        hero.physicsBody?.contactTestBitMask = groundGroup
+        hero.physicsBody?.contactTestBitMask = groundGroup | coinGroup | bigCoinGroup
         hero.physicsBody?.collisionBitMask = groundGroup
         
         hero.physicsBody?.isDynamic = true
@@ -168,5 +209,74 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func createHero() {
         addHero(heroNode: hero, atPosition: CGPoint(x: self.size.width / 4, y: 0 + flyHeroTex.size().height))
+    }
+    
+    @objc func addCoin() {
+        coin = SKSpriteNode(texture: coinTexture)
+               
+       
+        let coinAnimation = SKAction.animate(with: coinTexturesArray, timePerFrame: 0.1)
+        let coinRepeat = SKAction.repeatForever(coinAnimation)
+        coin.run(coinRepeat)
+               
+        let movementAmount = arc4random() % UInt32(self.frame.size.height / 2)
+        let pipeOffset = CGFloat(movementAmount) - self.frame.size.height / 4
+        coin.size.width = 40
+        coin.size.height = 40
+        coin.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: coin.size.width - 20, height: coin.size.height - 20))
+        coin.physicsBody?.restitution = 0
+        coin.position = CGPoint(x: self.size.width + 50, y: 0 + coinTexture.size().height + 130 + pipeOffset)
+               
+        let moveCoin = SKAction.moveBy(x: -self.frame.size.width * 2, y: 0, duration: 5)
+        let removeAction = SKAction.removeFromParent()
+        let coinMoveBgForever = SKAction.repeatForever(SKAction.sequence([moveCoin, removeAction]))
+        coin.run(coinMoveBgForever)
+               
+        coin.physicsBody?.isDynamic = false
+        coin.physicsBody?.categoryBitMask = coinGroup
+        coin.zPosition = 1
+        coinObject.addChild(coin)
+    }
+    
+    @objc func bigCoinAdd() {
+        bigCoin = SKSpriteNode(texture: bigCoinTexture)
+           
+        let bigCoinAnimation = SKAction.animate(with: coinTexturesArray, timePerFrame: 0.1)
+        let bigCoinRepeat = SKAction.repeatForever(bigCoinAnimation)
+        bigCoin.run(bigCoinRepeat)
+        
+        let movementAmount = arc4random() % UInt32(self.frame.size.height / 2)
+        let pipeOffset = CGFloat(movementAmount) - self.frame.size.height / 4
+        bigCoin.size.width = 40
+        bigCoin.size.height = 40
+        bigCoin.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: bigCoin.size.width - 10, height: bigCoin.size.height - 10))
+        bigCoin.physicsBody?.restitution = 0
+        bigCoin.position = CGPoint(x: self.size.width + 50, y: 0 + coinTexture.size().height + 90 + pipeOffset)
+        
+        let moveCoin = SKAction.moveBy(x: -self.frame.size.width * 2, y: 0, duration: 5)
+        let removeAction = SKAction.removeFromParent()
+        let coinMoveBgForever = SKAction.repeatForever(SKAction.sequence([moveCoin, removeAction]))
+        bigCoin.run(coinMoveBgForever)
+        bigCoin.setScale(1.3)
+        bigCoin.physicsBody?.isDynamic = false
+        bigCoin.physicsBody?.categoryBitMask = bigCoinGroup
+        bigCoin.zPosition = 1
+        bigCoinObject.addChild(bigCoin)
+    }
+    
+    func addTimer() {
+        timerAddCoin.invalidate()
+        timerAddBigCoin.invalidate()
+           
+        timerAddCoin = Timer.scheduledTimer(timeInterval: 2.83,
+                                            target: self,
+                                            selector: #selector(GameScene.addCoin),
+                                            userInfo: nil,
+                                            repeats: true)
+        timerAddBigCoin = Timer.scheduledTimer(timeInterval: 9.114,
+                                               target: self,
+                                               selector: #selector(GameScene.bigCoinAdd),
+                                               userInfo: nil,
+                                               repeats: true)
     }
 }
